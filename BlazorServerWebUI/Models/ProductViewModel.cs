71429af4303d35ac80d5d65ce7a09e7f5608ad55
codingdroplets <codingdroplets@HP-VICTUS>
2022-06-28 18:43:19 +0530

    API Gateway

---
+++ BlazorServerWebUI/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerWebUI.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
---
+++ BlazorServerWebUI/Program.cs
@@ -0,0 +1,28 @@
+using BlazorServerWebUI.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://apigateway") });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
